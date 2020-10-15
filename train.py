ecad68f521bde2bad122fec25ae032eca465f12e
Foppe <flundkvi@kth.se>
2020-10-15 12:00:58 +0200

    add checkpoint saving and sampling, implement loss function

--- train.py
+++ train.py
@@ -33,6 +33,7 @@ def main(args):
     net = net.to(device)
 
     start_epoch = 0
+    #TODO: add functionality for loading checkpoints here
 
     loss_function = torch.nn.NLLLoss() #TODO: replace with the real loss function used in realNVP/glow
     optimizer = optim.Adam(net.parameters(), lr=args.lr)
@@ -42,7 +43,7 @@ def main(args):
 
     for epoch in range(start_epoch, start_epoch + args.num_epochs):
         train(net, cifar_train,device, optimizer, loss_function, scheduler)
-        test(model, cifar_test, device, loss_function) #how often do we want to test?
+        test(model, cifar_test, device, loss_function, epoch) #how often do we want to test?
 
 @torch.enable_grad()
 def train(model, trainloader, device, optimizer, loss_function, scheduler):
@@ -55,6 +56,8 @@ def train(model, trainloader, device, optimizer, loss_function, scheduler):
         optimizer
         loss_function
     """
+
+
     net.train()
     for x, y in trainloader:
         x.to(device)
@@ -66,18 +69,49 @@ def train(model, trainloader, device, optimizer, loss_function, scheduler):
         scheduler.step()
 
 @torch.no_grad()
-def test(model, testloader, device, loss_function):
+def test(model, testloader, device, loss_function, epoch):
     global best_loss #keep track of best loss
     loss = 0
+    num_samples = 32 #should probably be an argument
+    #TODO: add average loss checker here, they use that in code for checkpointing
     for x,y in testloader:
         x = x.to(device)
         z, logdet = model(x)
         loss += loss_function(x, y)
+
+    if avg_loss < best_loss:
+        print("saving model..")
+        checkpoint_state = {
+            "model" : model.state_dict(),
+            "test_loss" : avg_loss,
+            "epoch": epoch
+        }
+        os.makedirs("checkpoints", exist_ok=True)
+        torch.save(checkpoint_state, "checkpoints/best.pth.tar") # save the model
+        best_loss = avg_loss
     
-#open question: should we add a sample function
+    #generate samples after each test (?)
+    sample_images = generate(model, num_samples, device)
+    os.makedirs('generated_imgs', exist_ok=True)
+    grid = torchvision.utils.make_grid(sample_images, nrow=num_samples ** 0.5)
+    torchvision.utils.save_image(grid, f"generated_imgs/epoch_{epoch}")
+    
+@torch.no_grad()
+def generate(model, n_samples, device, n_channels=3):
+    """
+    Generate samples from the model
+    args:
+        model: the network model
+        n_samples: amount of samples to generate
+        device: the device we run the model on
+        n_channels: the amount of channels for the output (usually 3, but on MNIST it's 1)
+    """
+    z = torch.randn((n_samples, n_channels, 32, 32), dtype=torch.float32, device=device)
+    x, _ model(z).reverse() #not sure how you guys implemented the reverse functionality
+    return x
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="DD2412 Mini-glow)
+    parser = argparse.ArgumentParser(description="DD2412 Mini-glow")
     parser.add_argument('--batch_size', default=512, type=int, help="minibatch size") #using CIFAR optimizations as default here
     parser.add_argument('--lr', default=0.001, help="learning rate")
     parser.add_argument('--amt_channels', '-C', default=512, help="amount of channels in the hidden layers")
