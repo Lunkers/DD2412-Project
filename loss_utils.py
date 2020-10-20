glue together model and train code, fix errors in average meter, loss function, and train scripts

loss_utils.py
@@ -27,7 +27,7 @@ class FlowNLL(nn.Module):
             logdet: the log-determinant
         """
         #log-likelihood of the (assumed) gaussian for p(z)
-        prior_log_likelihood = 0.5 * (z ** 2 +np.log(2 * np.pi))
+        prior_log_likelihood = -0.5 * (z ** 2 +np.log(2 * np.pi))
         prior_log_likelihood = prior_log_likelihood.flatten(1).sum(-1) - np.log(self.k) * np.prod(z.size()[:1]) 
         log_likelihood = prior_log_likelihood + logdet
         negative_log_likelihood = -log_likelihood.mean()
@@ -43,6 +43,6 @@ def bits_per_dimension(x, negative_log_likelihood):
         negative_log_likelihood: negative log-likelihood loss tensor
     """
     dimensions = np.prod(x.size()[1:])
-    bpd = nll / np.log(2 * dim)
+    bpd = negative_log_likelihood / np.log(2 * dimensions)
 
     return bpd 

model.py
@@ -41,7 +41,7 @@ class Glow(nn.Module):
 
     def reverse(self, x, eps=[], eps_std=None):
         logdet = 0
-        for i, current_block in enumerate(self.blocks[::-1]):
+        for i, current_block in enumerate(reversed(self.blocks)): # a bit more readable, but might be an unnecessary change 
             if i < self.levels - 1:
                 x = split_reverse(x, eps, eps_std)
             x, logdet = current_block.reverse(x, logdet)
@@ -63,9 +63,10 @@ class Block(nn.Module):
 
     def reverse(self, x, logdet=None):
         # don't think logdet is relevant for reverse it seems in the original code, it's mostly ignored
-        for flow in self.flows[::-1]:
+        # We should still return it though, because the reverse() method in the model expects it
+        for flow in reversed(self.flows):
             x, logdet = flow.reverse(x, logdet)
-        return x
+        return x, logdet
 
 
 class StepFlow(nn.Module):