3d0aa34a6324c6cb9656c3b6eb59d57fb5f20fc6
Foppe <flundkvi@kth.se>
2020-10-23 14:52:42 +0200

    fix

--- nn.py
+++ nn.py
@@ -9,8 +9,8 @@ class NN(nn.Module):
         self.conv2 = nn.Conv2d(out_channels, out_channels, ker_size_1)
         self.conv3 = nn.Conv2d(out_channels, in_channels, ker_size_3, padding=1)
         # zero initialization
-        torch.nn.init.normal_(self.conv1.weight, min=0, std=0.05)
-        torch.nn.init.normal_(self.conv2.weight, min=0, std=0.05)
+        torch.nn.init.normal_(self.conv1.weight, mean=0, std=0.05)
+        torch.nn.init.normal_(self.conv2.weight, mean=0, std=0.05)
         torch.nn.init.zeros_(self.conv3.weight)
         torch.nn.init.zeros_(self.conv3.bias)
         torch.nn.init.zeros_(self.conv1.bias)
